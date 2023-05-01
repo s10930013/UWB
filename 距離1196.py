b8e4177dcb3ca671ff6b425339d514486c1c4640
UWB <s10930013@gm.cyut.edu.tw>
2023-05-02 04:18:28 +0800

    add

--- 距離1196.py
+++ 距離1196.py
@@ -3,6 +3,7 @@ import time
 import openpyxl
 n=input("請輸入偵測次數")
 int (i =0)
+a=[]
 def distance1196():
     while  i<n:
         response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
@@ -10,8 +11,8 @@ def distance1196():
         str2= str1[1].split('"')
         str3= str2[5].split('\\')
         print(int(str3[0]))
+        a[i]=(str3)
 
 
-        
         time.sleep(1)
         i+=1
