6a582b7ab67130e1ba6b588c0800965f54b68a50
UWB <s10930013@gm.cyut.edu.tw>
2023-05-02 04:13:33 +0800

    add

--- 距離1196.py
+++ 距離1196.py
@@ -1,12 +1,17 @@
 import requests
 import time
 import openpyxl
+n=input("請輸入偵測次數")
+int (i =0)
 def distance1196():
-    while 1:
+    while  i<n:
         response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
         str1 = response.text.split('Tag1196')
         str2= str1[1].split('"')
         str3= str2[5].split('\\')
-
         print(int(str3[0]))
+
+
+        
         time.sleep(1)
+        i+=1
--- 距離匯入excel.py
+++ 距離匯入excel.py
@@ -1,14 +1,9 @@
 import requests
 import time
-from openpyxl import Workbook,load_workbook
-wb = load_workbook('109.xlsx')
-ws = wb.active
-print(ws['A2'].value)
-while 7==7:
-    response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
-    str1 = response.text.split('Tag1196')
-    str2= str1[1].split('"')
-    str3= str2[5].split('\\')
-    x=int(str3[0])
-    print(x)
-    time.sleep(1.5)
+import openpyxl
+import csv
+import 距離1196
+距離1196.distance1196()
+with open("xy.csv",mode="w",newline="") as file:
+    writer=csv.write(file)
+    writer.writerow([1,2,3])
