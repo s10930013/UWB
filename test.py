eed958386ece2ca7468f4889daea16ef6a2fbd11
s10930013 <s10930013@gm.cyut.edu.tw>
2023-05-30 14:34:53 +0800

    add

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import time
 import math
 import openpyxl
 import os
+import csv
 import 角度1196
 import 距離1196
 import 座標1196
@@ -13,13 +14,22 @@ while i<n:
     ##角度轉弧度
     a1196=math.radians(角度1196.angle1196())
     d1196=距離1196.distance1196()
-    x,y=座標1196.xy1196(d1196,a1196)
+    x,y,x1196,y1196=座標1196.xy1196(d1196,a1196)
     print(x,y)
-    
     畫圖1.a1(x,y)
 
 
     i+=1
     time.sleep(1)
-    
+##******座標數據儲存******
+x1196.insert(0, "X座標")
+y1196.insert(0, "Y座標")
+data = list(zip(x1196, y1196))
 
+with open('1196.csv', "w", newline='') as s:
+    # 創建 CSV 寫入器
+    w = csv.writer(s)
+
+    # 將配對後的資料逐列寫入 CSV 檔案
+    w.writerows(data)
+    
