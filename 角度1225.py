1adc2bd3724b5a5521d75700b8a596fbce25378a
UWB <s10930013@gm.cyut.edu.tw>
2023-12-02 07:02:28 +0800

    adddddddddddd

--- 角度1225.py
+++ 角度1225.py
@@ -1,11 +1,11 @@
 import requests
 def angle1225():
 
-    response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
-    str1 = response.text.split('Tag1225')
-    str2= str1[1].split('"')
-    str3= str2[9].split('\\')
-    a1225=float(str3[0])
+    #response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
+    #str1 = response.text.split('Tag1225')
+    #str2= str1[1].split('"')
+    #str3= str2[9].split('\\')
+    a1225=60#float(str3[0])
     #print(a1196)
     
     return a1225
--- 距離1196.py
+++ 距離1196.py
@@ -3,12 +3,12 @@ a=[]
 def distance1196():
     
     response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
-    str1 = response.text.split('Tag1196')
-    str2= str1[1].split('"')
-    str3= str2[5].split('\\')
+    #str1 = response.text.split('Tag1196')
+    #str2= str1[1].split('"')
+    #str3= str2[5].split('\\')
     #print(int(str3[0]))
-    a.append(int(str3[0]))
-    d1196=int(str3[0])
+    #a.append(int(str3[0]))
+    d1196=100#int(str3[0])
 
 
 
