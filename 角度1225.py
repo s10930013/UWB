47bed65f68265ae1451a89d59c3c5be4282a7381
UWB <s10930013@gm.cyut.edu.tw.>
2024-03-20 11:14:53 +0800

    ad

--- 角度1225.py
+++ 角度1225.py
@@ -1,13 +1,13 @@
 import requests
 def angle1225():
-    
+    '''
     response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
     str1 = response.text.split('Tag1225')
     str2= str1[1].split('"')
     str3= str2[9].split('\\')
     a1225=float(str3[0])
     #print(a1225)
-    
-    #a1225=float(50)
+    '''
+    a1225=float(50)
 
     return a1225
--- 距離1225.py
+++ 距離1225.py
@@ -2,7 +2,7 @@ import requests
 import random
 a=[]
 def distance1225():
-    
+    '''
     response = requests.get('https://norgay.center/php/vilsnodes.php?loadranging=1&project_id=1')
     str1 = response.text.split('Tag1225')
     str2= str1[1].split('"')
@@ -10,8 +10,8 @@ def distance1225():
     print(int(str3[0]))
     a.append(int(str3[0]))
     d1225=int(str3[0])
-    
+    '''
 
-    #d1225=int(random.randint(100,700))
+    d1225=int(random.randint(100,700))
     return d1225
     
