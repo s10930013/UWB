c394639fa37a8bed0d68797958a2d84ce250c500
UWB <s10930013@gm.cyut.edu.tw>
2024-04-15 15:53:01 +0800

    ASDSAD

--- 測量結果/準確率.py
+++ 測量結果/準確率.py
@@ -12,7 +12,7 @@ def calculate_accuracy(csv_file):
             x, y, _, _ = row  
             x = float(x)
             y = float(y)
-            if 270 < x < 330 and 270 < y < 330:  
+            if 460 < x < 540 and 460 < y < 540:  
                 inside_range_count += 1
 
     if total_count == 0:
@@ -20,6 +20,6 @@ def calculate_accuracy(csv_file):
     else:
         return inside_range_count / total_count
 
-csv_file = 'c:/Users/weiwe/Desktop/UWB/測量結果/D2.csv'
+csv_file = 'C:/Users/20171226/Desktop/UWB/測量結果/C2.csv'
 accuracy = calculate_accuracy(csv_file)
 print(f"B1.csv 文件中tag的準確率: {accuracy}")
